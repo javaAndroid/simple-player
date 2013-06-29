bb2ce1870042358c9bcb3f65930675a066edf020
javaAndroid <sveridovdenys@yandex.ua>
2013-06-29 14:05:33 +0300

    *********

--- src/com/example/simple_player/MyActivity.java
+++ src/com/example/simple_player/MyActivity.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 public class MyActivity extends Activity implements View.OnClickListener {
 
     private final String TAG = "Player";
-    private ArrayList<String> playList;
+    private ArrayList<String> playList, nameAudioList;
     private String foramt = "mp3";
     private ListView listAudio;
     private MediaPlayer mp;
@@ -29,20 +29,25 @@ public class MyActivity extends Activity implements View.OnClickListener {
     private ProgressDialog prgDialog;
     private SeekBar prgAudio;
     private Timer time;
-    private TextView timeOfPlay , timeAudio;
+    private TextView timeOfPlay, timeAudio, tvNameAuthor, tvNameAudio;
     private SeekBarHandler handler = new SeekBarHandler();
-    private Handler handler112 = new Handler(){
+    private Handler handler112 = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             timeAudio.setText(msg.getData().getString("time"));
         }
     };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        tvNameAuthor = (TextView) findViewById(R.id.tvNameAuthor);
+        tvNameAudio = (TextView) findViewById(R.id.tvNameAudio);
+
+        nameAudioList = new ArrayList<String>();
 
         timeOfPlay = (TextView) findViewById(R.id.timeOfPlay);
         timeAudio = (TextView) findViewById(R.id.timeAudio);
@@ -59,21 +64,28 @@ public class MyActivity extends Activity implements View.OnClickListener {
         final Handler handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MyActivity.this, android.R.layout.simple_expandable_list_item_1, android.R.id.text1, playList);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MyActivity.this, android.R.layout.simple_expandable_list_item_1, android.R.id.text1, nameAudioList);
                 listAudio.setAdapter(adapter);
 
                 listAudio.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        MyActivity.this.position = position;
-                        TextView t = (TextView) view.findViewById(android.R.id.text1);
-                        Log.d(TAG, t.getText().toString());
-                        play(t.getText().toString());
+
+//                        TextView t = (TextView) view.findViewById(android.R.id.text1);
+//                        t.setTextColor(0xff343434);
+//                        Log.d(TAG, t.getText().toString());
+//                        setTitle(nameAudioList.get(position));
+                        play(playList.get(position));
+
+//                        View w = (View) listAudio.getAdapter().getView(position, null, null);
+//                        t = (TextView) w.findViewById(android.R.id.text1);
+//                        w.setBackgroundColor(0xff123212);
+//                        MyActivity.this.position = position;
                     }
                 });
             }
         };
-        final Handler handler1 = new Handler(){
+        final Handler handler1 = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
@@ -95,6 +107,27 @@ public class MyActivity extends Activity implements View.OnClickListener {
         btnPlay = (Button) findViewById(R.id.btnPlay);
         btnPlay.setOnClickListener(this);
 
+
+        prgAudio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                time.setContinuee(true);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                mp.seekTo(prgAudio.getProgress() * 1000);
+                time.setSeconds(prgAudio.getProgress());
+                time.setContinuee(false);
+            }
+        });
+
     }
 
     public void search(File file) {
@@ -114,12 +147,18 @@ public class MyActivity extends Activity implements View.OnClickListener {
             String formatFile = file.getName();
             formatFile = formatFile.substring(formatFile.length() - 3);
             Log.d(TAG, formatFile);
+
+
            /*
            Если формат подходит забрасываем в список
             */
             if (formatFile.equals(foramt)) {
                 ++size;
                 playList.add(file.getPath());
+
+                String fileName = file.getName();
+                String name[] = fileName.split("/");
+                nameAudioList.add(name[name.length - 1]);
             }
         }
     }
@@ -136,12 +175,22 @@ public class MyActivity extends Activity implements View.OnClickListener {
             mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override
                 public void onPrepared(MediaPlayer mp) {
-                    final byte minutes = (byte) ((mp.getDuration()/1000)/60);
-                    final byte seconds = (byte) ((mp.getDuration()/1000)%60);
+                    final byte minutes = (byte) ((mp.getDuration() / 1000) / 60);
+                    final byte seconds = (byte) ((mp.getDuration() / 1000) % 60);
 
                     Message msg = new Message();
                     Bundle b = new Bundle();
-                    b.putString("time" , minutes+":"+seconds);
+                    String time = new String();
+                    if (minutes < 10)
+                        time += 0;
+                    time += minutes;
+                    time += ':';
+                    if (seconds < 10) {
+                        time += 0;
+                    }
+                    time += seconds;
+
+                    b.putString("time", time);
                     msg.setData(b);
                     handler112.sendMessage(msg);
                 }
@@ -169,24 +218,28 @@ public class MyActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-       if(v.getId() == btnPlay.getId()){
-           if (mp.isPlaying()) {
-               mp.pause();
-               btnPlay.setText(getResources().getString(R.string.play));
-               if (time != null) {
-                   time.setPause(true);
-               }
-           } else {
-               mp.start();
-               btnPlay.setText(getResources().getString(R.string.pause));
-               if (time != null) {
-                   time.setPause(false);
-               }
-           }
-       }
+        if (v.getId() == btnPlay.getId()) {
+            if (mp.isPlaying()) {
+                mp.pause();
+                btnPlay.setText(getResources().getString(R.string.play));
+                if (time != null) {
+                    time.setPause(true);
+                }
+            } else {
+                mp.start();
+                btnPlay.setText(getResources().getString(R.string.pause));
+                if (time != null) {
+                    time.setPause(false);
+                }
+            }
+        }
     }
 
-
+    public void setTitle(String fileName) {
+        String[] info = fileName.split("-");
+        tvNameAuthor.setText(info[0]);
+        tvNameAudio.setText(info[1]);
+    }
 
     // Classes
 
@@ -195,6 +248,9 @@ public class MyActivity extends Activity implements View.OnClickListener {
         private Handler handler;
         private boolean stop;
         private boolean pause;
+        private boolean continuee;
+
+        private int second;
 
         Timer(int time, Handler handler) {
             this.time = time;
@@ -217,9 +273,21 @@ public class MyActivity extends Activity implements View.OnClickListener {
             return pause;
         }
 
+        public void setContinuee(boolean value) {
+            continuee = value;
+        }
+
+        public void setSeconds(int seconds) {
+            second = seconds;
+        }
+
+        public boolean getContinuee() {
+            return continuee;
+        }
+
         @Override
         public void run() {
-            int second = 0;
+
 
             while (second <= time && !stop) {
                 if (pause)
@@ -230,27 +298,30 @@ public class MyActivity extends Activity implements View.OnClickListener {
 
                 }
                 ++second;
-                handler.sendEmptyMessage(second);
+                if (!continuee)
+                    handler.sendEmptyMessage(second);
             }
         }
     }
+
     class SeekBarHandler extends Handler {
         byte minutes;
         byte seconds;
+
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             prgAudio.setProgress(msg.what);
-             minutes = (byte) (msg.what/60);
-            seconds = (byte) (msg.what%60);
+            minutes = (byte) (msg.what / 60);
+            seconds = (byte) (msg.what % 60);
             String time = new String();
-            if(minutes < 10)
+            if (minutes < 10)
                 time += 0;
-             time+= minutes;
-             time+=':';
-              if(seconds < 10)
-                  time+=0;
-            time+=seconds;
+            time += minutes;
+            time += ':';
+            if (seconds < 10)
+                time += 0;
+            time += seconds;
             timeOfPlay.setText(time);
         }
     }
